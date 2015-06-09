74569f4da8d58f5fd0c0618ee1a613ed728adaa6
Roland Kaiser <kardinal.eros@gmail.com>
2015-06-09 18:59:30 +0200

    updated install script

--- install/BuildCheckAndInstall.sh
+++ install/BuildCheckAndInstall.sh
@@ -1,3 +1,3 @@
 R CMD build --no-build-vignettes /Users/roli/Documents/voucher/pkg
 R CMD check voucher_0.1-2.tar.gz
-R CMD INSTALL -l /Users/roli/Library/R/3.1/library voucher_0.1-2.tar.gz
+R CMD INSTALL -l /Users/roli/Library/R/3.2/library voucher_0.1-2.tar.gz
